4ce462e2f24f339ce04a0d7dd2bd9ba83f1f29f9
jirishus <jirishus@yahoo.com>
2015-08-03 14:09:19 -0700

    removed main struct tags

--- phone.go
+++ phone.go
@@ -6,182 +6,176 @@ import (
 )
 
 type ID struct {
-	Key 		string `json:"key"`
-	URL 		string `json:"url"`
-	Type 		string `json:"type"`
-	UUID 		string `json:"uuid"`
-	Durability 	string `json:"durability"`	
-} `json:"id"`
+	Key        string `json:"key"`
+	URL        string `json:"url"`
+	Type       string `json:"type"`
+	UUID       string `json:"uuid"`
+	Durability string `json:"durability"`
+}
 
 type V2PhoneResponse struct {
-	ID ID `json:"id"`
-	LineType 			string `json:"line_type"`
-	IsValid 			bool `json:"is_valid"`
-	PhoneNumber 		string `json:"phone_number"`
-	CountryCallingCode 	string `json:"country_calling_code"`
-	Extension 			string `json:"extension"`
-	Carrier 			string `json:"carrier"`
-	DoNotCall			bool `json:"do_not_call"`
-	IsPrepaid 			bool `json:"is_prepaid"`
-	IsConnected 		bool `json:"is_connected"`
-} `json:"v2_phone_response"`
+	ID                 ID     `json:"id"`
+	LineType           string `json:"line_type"`
+	IsValid            bool   `json:"is_valid"`
+	PhoneNumber        string `json:"phone_number"`
+	CountryCallingCode string `json:"country_calling_code"`
+	Extension          string `json:"extension"`
+	Carrier            string `json:"carrier"`
+	DoNotCall          bool   `json:"do_not_call"`
+	IsPrepaid          bool   `json:"is_prepaid"`
+	IsConnected        bool   `json:"is_connected"`
+}
 
 type Name struct {
-	Salutation 	string `json:"salutation"`
-	FirstName 	string `json:"first_name"`
-	MiddleName 	string `json:"middle_name"`
-	LastName 	string `json:"last_name"`
-	Suffix		string `json:"suffix"`
-	ValidFor 	bool `json:"valid_for"`
-} `json:"name"`
+	Salutation string `json:"salutation"`
+	FirstName  string `json:"first_name"`
+	MiddleName string `json:"middle_name"`
+	LastName   string `json:"last_name"`
+	Suffix     string `json:"suffix"`
+	ValidFor   bool   `json:"valid_for"`
+}
 
 type LatLong struct {
-	Latitude 	float64 `json:"is_deliverable"`
-	Longitude 	float64 `json:"longitude"`
-	Accuracy 	string `json:"accuracy"`	
-} `json:"lat_long"`
+	Latitude  float64 `json:"is_deliverable"`
+	Longitude float64 `json:"longitude"`
+	Accuracy  string  `json:"accuracy"`
+}
 
 type Location struct {
-	ID ID `json:"id"`
-	Type string `json:"type"`
+	ID       ID     `json:"id"`
+	Type     string `json:"type"`
 	ValidFor struct {
 		Start struct {
-			Year int `json:"year"`
+			Year  int `json:"year"`
 			Month int `json:"month"`
-			Day int `json:"day"`
+			Day   int `json:"day"`
 		} `json:"start"`
 		Stop struct {
-			Year int `json:"year"`
+			Year  int `json:"year"`
 			Month int `json:"month"`
-			Day int `json:"day"`
+			Day   int `json:"day"`
 		} `json:"stop"`
 	} `json:"valide_for"`
-	Latlong LatLong
-	IsDeliverable 			bool `json:"is_deliverable"`
-	StandardAddressLine1 	string `json:"standard_address_line1"`
-	StandardAddressLine2 	string `json:"standard_address_line2"`
+	Latlong                 LatLong
+	IsDeliverable           bool   `json:"is_deliverable"`
+	StandardAddressLine1    string `json:"standard_address_line1"`
+	StandardAddressLine2    string `json:"standard_address_line2"`
 	StandardAddressLocation string `json:"standard_address_location"`
-	IsHistorical 			bool `json:"is_historical"`
-	ContactType 			string `json:"contact_type"`
-	ContactCreationDate 	int `json:"contact_creation_date"`
-
-} `json:"location"`
+	IsHistorical            bool   `json:"is_historical"`
+	ContactType             string `json:"contact_type"`
+	ContactCreationDate     int    `json:"contact_creation_date"`
+}
 
 type BelongsTo []struct {
-	ID ID `json:"id"`
-	Type string `json:"type"`
-	Names []Name `json:"names"`
+	ID       ID     `json:"id"`
+	Type     string `json:"type"`
+	Names    []Name `json:"names"`
 	AgeRange struct {
-		Start 	int `json:"start"`
-		End 	int `json:"end"`
+		Start int `json:"start"`
+		End   int `json:"end"`
 	} `json:"age_range"`
-	Gender 			string `json:"gender"`
-	Locations []Location `json:"locations"`
-	LegalEntitiesAt string `json:"legal_entities_at"`
-	City 			string `json:"city"`
-	PostalCode 		string `json:"postal_code"`
-	Zip4 			string `json:"zip4"`
-	StateCode 		string `json:"state_code"`
-	CountryCode 	string `json:"country_code"`
-	Address 		string `json:"address"`
-	House 			string `json:"house"`
-	StreetName 		string `json:"street_name"`
-	StreetType 		string `json:"street_type"`
-	PreDir 			string `json:"pre_dir"`
-	PostDir 		string `json:"post_dir"`
-	AptNumber 		string `json:"apt_number"`
-	AptType 		string `json:"apt_type"`
-	BoxNumber 		string `json:"box_number"`
-	IsReceivingMail bool `json:"is_receiving_mail"`
-	NotReceivingMailReason bool `json:"not_receiving_mail_reason"`
-	Usage 			string `json:"usage"`
-	DeliveryPoint 	string `json:"delivery_point"`
-	BoxType 		string `json:"box_type"`
-	AddressType 	string `json:"address_type"`
-} `json:"belongs_to"`
-
+	Gender                 string     `json:"gender"`
+	Locations              []Location `json:"locations"`
+	LegalEntitiesAt        string     `json:"legal_entities_at"`
+	City                   string     `json:"city"`
+	PostalCode             string     `json:"postal_code"`
+	Zip4                   string     `json:"zip4"`
+	StateCode              string     `json:"state_code"`
+	CountryCode            string     `json:"country_code"`
+	Address                string     `json:"address"`
+	House                  string     `json:"house"`
+	StreetName             string     `json:"street_name"`
+	StreetType             string     `json:"street_type"`
+	PreDir                 string     `json:"pre_dir"`
+	PostDir                string     `json:"post_dir"`
+	AptNumber              string     `json:"apt_number"`
+	AptType                string     `json:"apt_type"`
+	BoxNumber              string     `json:"box_number"`
+	IsReceivingMail        bool       `json:"is_receiving_mail"`
+	NotReceivingMailReason bool       `json:"not_receiving_mail_reason"`
+	Usage                  string     `json:"usage"`
+	DeliveryPoint          string     `json:"delivery_point"`
+	BoxType                string     `json:"box_type"`
+	AddressType            string     `json:"address_type"`
+}
 
 type AssociatedLocations []struct {
-	ID ID `json:"id"`
-	Type 					string `json:"type"`
-	ValidFor 				string `json:"valid_for"`
-	LegalEntitiesAt 		string `json:"legal_entities_at"`
-	City 					string `json:"city"`
-	PostalCode 				string `json:"postal_code"`
-	Zip4 					string `json:"zip4"`
-	StateCode 				string `json:"state_code"`
-	CountryCode 			string `json:"country_code"`
-	Address 				string `json:"address"`
-	House 					string `json:"house"`
-	StreetName 				string `json:"street_name"`
-	StreetType 				string `json:"street_type"`
-	PreDir 					string `json:"pre_dir"`
-	PostDir 				string `json:"post_dir"`
-	AptNumber 				string `json:"apt_number"`
-	AptType 				string `json:"apt_type"`
-	BoxNumber 				string `json:"box_number"`
-	IsReceivingMail 		string `json:"is_receiving_mail"`
-	NotReceivingMailReason 	string `json:"not_receiving_mail_reason"`
-	Usage 				   	string `json:"usage"`
-	DeliveryPoint 			string `json:"delivery_point"`
-	BoxType 				string `json:"box_type"`
-	AddressType 			string `json:"address_type"`
-	LatLong LatLong
-	IsDeliverable 			bool `json:"is_deliverable"`
-	StandardAddressLine1 	string `json:"standard_address_line1"`
-	StandardAddressLine2 	string `json:"standard_address_line2"`
+	ID                      ID     `json:"id"`
+	Type                    string `json:"type"`
+	ValidFor                string `json:"valid_for"`
+	LegalEntitiesAt         string `json:"legal_entities_at"`
+	City                    string `json:"city"`
+	PostalCode              string `json:"postal_code"`
+	Zip4                    string `json:"zip4"`
+	StateCode               string `json:"state_code"`
+	CountryCode             string `json:"country_code"`
+	Address                 string `json:"address"`
+	House                   string `json:"house"`
+	StreetName              string `json:"street_name"`
+	StreetType              string `json:"street_type"`
+	PreDir                  string `json:"pre_dir"`
+	PostDir                 string `json:"post_dir"`
+	AptNumber               string `json:"apt_number"`
+	AptType                 string `json:"apt_type"`
+	BoxNumber               string `json:"box_number"`
+	IsReceivingMail         string `json:"is_receiving_mail"`
+	NotReceivingMailReason  string `json:"not_receiving_mail_reason"`
+	Usage                   string `json:"usage"`
+	DeliveryPoint           string `json:"delivery_point"`
+	BoxType                 string `json:"box_type"`
+	AddressType             string `json:"address_type"`
+	LatLong                 LatLong
+	IsDeliverable           bool   `json:"is_deliverable"`
+	StandardAddressLine1    string `json:"standard_address_line1"`
+	StandardAddressLine2    string `json:"standard_address_line2"`
 	StandardAddressLocation string `json:"standard_address_location"`
-	IsHistorical 			bool `json:"is_historical"`
-	ContactType 			string `json:"contact_type"`
-	ContactCreationDate 	string `json:"contact_creation_date"`
-} `json:"associated_locations"`
-
+	IsHistorical            bool   `json:"is_historical"`
+	ContactType             string `json:"contact_type"`
+	ContactCreationDate     string `json:"contact_creation_date"`
+}
 
 type Reputation struct {
-	SpamScore 	int `json:"spam_score"`
-	SpamIndex 	int `json:"spam_index`
-	Level		int `json:"level"`
-	Details []struct {
-		Score 		int `json:"score"`
-		Type 		int `json:"type"`
-		Category 	int `json:"category"`
+	SpamScore int `json:"spam_score"`
+	SpamIndex int `json:"spam_index`
+	Level     int `json:"level"`
+	Details   []struct {
+		Score    int `json:"score"`
+		Type     int `json:"type"`
+		Category int `json:"category"`
 	} `json:"details"`
-} `json:"reputation"`
+}
 
 type BestLocation struct {
-	ID ID `json:"id"`
-	Type 					string `json:"type"`
-	ValidFor 				string `json:"valid_for"`
-	LegalEntitiesAt 		string `json:"legal_entities_at"`
-	City 					string `json:"city"`
-	PostalCode 				string `json:"postal_code"`
-	Zip4 					string `json:"zip4"`
-	StateCode 				string `json:"state_code"`
-	CountryCode 			string `json:"country_code"`
-	Address 				string `json:"address"`
-	House 					string `json:"house"`
-	StreetName 				string `json:"street_name"`
-	StreetType 				string `json:"street_type"`
-	PreDir 					string `json:"pre_dir"`
-	PostDir 				string `json:"post_dir"`
-	AptNumber 				string `json:"apt_number"`
-	AptType 				string `json:"apt_type"`
-	BoxNumber 				string `json:"box_number"`
-	IsReceivingMail 		string `json:"is_receiving_mail"`
-	NotReceivingMailReason 	string `json:"not_receiving_mail_reason"`
-	Usage 					string `json:"usage"`
-	DeliveryPoint 			string `json:"delivery_point"`
-	BoxType 				string `json:"box_type"`
-	AddressType 			string `json:"address_type"`
-	LatLong LatLong
-	IsDeliverable 			bool `json:"is_deliverable"`
-	StandardAddressLine1 	string `json:"standard_address_line1"`
-	StandardAddressLine2 	string `json:"standard_address_line2"`
+	ID                      ID     `json:"id"`
+	Type                    string `json:"type"`
+	ValidFor                string `json:"valid_for"`
+	LegalEntitiesAt         string `json:"legal_entities_at"`
+	City                    string `json:"city"`
+	PostalCode              string `json:"postal_code"`
+	Zip4                    string `json:"zip4"`
+	StateCode               string `json:"state_code"`
+	CountryCode             string `json:"country_code"`
+	Address                 string `json:"address"`
+	House                   string `json:"house"`
+	StreetName              string `json:"street_name"`
+	StreetType              string `json:"street_type"`
+	PreDir                  string `json:"pre_dir"`
+	PostDir                 string `json:"post_dir"`
+	AptNumber               string `json:"apt_number"`
+	AptType                 string `json:"apt_type"`
+	BoxNumber               string `json:"box_number"`
+	IsReceivingMail         string `json:"is_receiving_mail"`
+	NotReceivingMailReason  string `json:"not_receiving_mail_reason"`
+	Usage                   string `json:"usage"`
+	DeliveryPoint           string `json:"delivery_point"`
+	BoxType                 string `json:"box_type"`
+	AddressType             string `json:"address_type"`
+	LatLong                 LatLong
+	IsDeliverable           bool   `json:"is_deliverable"`
+	StandardAddressLine1    string `json:"standard_address_line1"`
+	StandardAddressLine2    string `json:"standard_address_line2"`
 	StandardAddressLocation string `json:"standard_address_location"`
-
-} `json:"best_location"`
-
-
+}
 
 func (c *V2Client) Phone(params map[string]string, timeout time.Duration) (error, V2PhoneResponse) {
 	p := V2PhoneResponse{}
