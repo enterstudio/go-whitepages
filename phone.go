d6feefc1aff5aed28fac4e9581aa7ef556af2778
jirishus <jirishus@yahoo.com>
2015-08-04 11:15:57 -0700

    wp-phone locations complete

--- phone.go
+++ phone.go
@@ -42,6 +42,7 @@ type BelongsTo []struct {
 	AgeRange  `json:"age_range"`
 	Gender    string `json:"gender"`
 	Locations `json:"locations"`
+	Phones    `json:"phones"`
 }
 
 type Names []struct {
@@ -59,8 +60,44 @@ type AgeRange []struct {
 }
 
 type Locations []struct {
-	Type     string `json:"type"`
-	ValidFor `json:"valid_for"`
+	ID struct {
+		Key        string `json:"key"`
+		URL        string `json:"url"`
+		Type       string `json:"type"`
+		UUID       string `json:"uuid"`
+		Durability string `json:"durability"`
+	} `json:"id"`
+	Type                    string `json:"type"`
+	ValidFor                `json:"valid_for"`
+	LatLong                 `json:"lat_long"`
+	LegalEntitiesAt         string `json:"legal_entities_at"`
+	City                    string `json:"city"`
+	PostalCode              string `json:"postal_code"`
+	Zip4                    string `json:"zip4"`
+	StateCode               string `json:"state_code"`
+	CountryCode             string `json:"country_code"`
+	Address                 string `json:"address"`
+	House                   string `json:"house"`
+	StreetName              string `json:"street_name"`
+	StreetType              string `json:"street_type"`
+	PreDir                  string `json:"pre_dir"`
+	PostDir                 string `json:"post_dir"`
+	AptNumber               string `json:"apt_number"`
+	AptType                 string `json:"apt_type"`
+	BoxNumber               string `json:"box_number"`
+	IsReceivingMail         bool   `json:"is_receiving_mail"`
+	NotReceivingMailReason  string `json:"not_receiving_mail_reason"`
+	Usage                   string `json:"usage"`
+	DeliveryPoint           string `json:"delivery_point"`
+	BoxType                 string `json:"box_type"`
+	AddressType             string `json:"address_type"`
+	IsDeliverable           bool   `json:"is_deliverable"`
+	StandardAddressLine1    string `json:"standard_address_line1"`
+	StandardAddressLine2    string `json:"standard_address_line2"`
+	StandardAddressLocation string `json:"standard_address_location"`
+	IsHistorical            bool   `json:"is_historical"`
+	ContactType             string `json:"contact_type"`
+	ContactCreationDate     int    `json:"contact_creation_date"`
 }
 
 type ValidFor struct {
@@ -74,6 +111,16 @@ type ValidFor struct {
 		Month int `json:"month"`
 		Day   int `json:"day"`
 	} `json:"stop"`
+}
+
+type LatLong struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	Accuracy  string  `json:"accuracy"`
+}
+
+type Phones []struct {
+	LineType string `json:"line_type"`
 }
 
 func (c *V2Client) Phone(params map[string]string, timeout time.Duration) (error, V2PhoneResponse) {
