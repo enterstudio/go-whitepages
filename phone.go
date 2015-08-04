96b7d39b7cb00bbc78be2fcfbeaf62acff773729
jirishus <jirishus@yahoo.com>
2015-08-04 12:01:47 -0700

    wp associatedLocations complete

--- phone.go
+++ phone.go
@@ -17,16 +17,17 @@ type Results []struct {
 		UUID       string `json:"uuid"`
 		Durability string `json:"durability"`
 	}
-	LineType           string `json:"line_type"`
-	BelongsTo          `json:"belongs_to"`
-	IsConnected        bool   `json:"is_connected"`
-	IsValid            bool   `json:"is_valid"`
-	PhoneNumber        string `json:"phone_number"`
-	CountryCallingCode string `json:"country_calling_code"`
-	Exension           string `json:"extension"`
-	Carrier            string `json:"carrier"`
-	DoNotCall          bool   `json:"do_not_call"`
-	IsPrepaid          bool   `json:"is_prepaid"`
+	LineType            string `json:"line_type"`
+	BelongsTo           `json:"belongs_to"`
+	AssociatedLocations `json:"associated_locations"`
+	IsConnected         bool   `json:"is_connected"`
+	IsValid             bool   `json:"is_valid"`
+	PhoneNumber         string `json:"phone_number"`
+	CountryCallingCode  string `json:"country_calling_code"`
+	Exension            string `json:"extension"`
+	Carrier             string `json:"carrier"`
+	DoNotCall           bool   `json:"do_not_call"`
+	IsPrepaid           bool   `json:"is_prepaid"`
 }
 
 type BelongsTo []struct {
@@ -196,6 +197,47 @@ type BestLocation struct {
 	StandardAddressLine1    string `json:"standard_address_line1"`
 	StandardAddressLine2    string `json:"standard_address_line2"`
 	StandardAddressLocation string `json:"standard_address_location"`
+}
+
+type AssociatedLocations []struct {
+	ID struct {
+		Key        string `json:"key"`
+		URL        string `json:"url"`
+		Type       string `json:"type"`
+		UUID       string `json:"uuid"`
+		Durability string `json:"durability"`
+	} `json:"id"`
+	Type                    string `json:"type"`
+	ValidFor                string `json:"valid_for"`
+	LegalEntitiesAt         string `json:"legal_entities_at"`
+	City                    string `json:"city"`
+	PostalCode              string `json:"postal_code"`
+	Zip4                    string `json:"zip4"`
+	StateCode               string `json:"state_code"`
+	CountryCode             string `json:"country_code"`
+	Address                 string `json:"address"`
+	House                   string `json:"house"`
+	StreetName              string `json:"street_name"`
+	StreetType              string `json:"street_type"`
+	PreDir                  string `json:"pre_dir"`
+	PostDir                 string `json:"post_dir"`
+	AptNumber               string `json:"apt_number"`
+	AptType                 string `json:"apt_type"`
+	BoxNumber               string `json:"box_number"`
+	IsReceivingMail         bool   `json:"is_receiving_mail"`
+	NotReceivingMailReason  string `json:"not_receiving_mail_reason"`
+	Usage                   string `json:"usage"`
+	DeliveryPoint           string `json:"delivery_point"`
+	BoxType                 string `json:"box_type"`
+	AddressType             string `json:"address_type"`
+	LatLong                 `json:"lat_long"`
+	IsDeliverable           bool   `json:"is_deliverable"`
+	StandardAddressLine1    string `json:"standard_address_line1"`
+	StandardAddressLine2    string `json:"standard_address_line2"`
+	StandardAddressLocation string `json:"standard_address_location"`
+	IsHistorical            bool   `json:"is_historical"`
+	ContactType             string `json:"contact_type"`
+	ContactCreationDate     string `json:"contact_creation_date"`
 }
 
 func (c *V2Client) Phone(params map[string]string, timeout time.Duration) (error, V2PhoneResponse) {
