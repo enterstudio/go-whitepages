21735d38506bd1716985f7ee449953c17747f6b2
Eric Greene <eric.greene@thecontrolgroup.com>
2016-04-01 23:26:32 -0700

    updated to use go-concierge. works. still need to update tests

--- client.go
+++ client.go
@@ -3,11 +3,11 @@ package whitepages
 import (
 	"encoding/json"
 	"errors"
-	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 	"time"
+
+	"github.com/the-control-group/go-concierge"
 )
 
 const (
@@ -18,7 +18,7 @@ const (
 )
 
 type Client struct {
-	ApiKey    string
+	APIKey    string
 	Version   string
 	BaseURL   string
 	UserAgent string
@@ -34,70 +34,96 @@ type ErrorResponse struct {
 
 func NewClient(key string) *Client {
 	c := &Client{}
-	c.ApiKey = key
+	c.APIKey = key
 	baseURL := defaultBaseURL + version + "/"
 	c.BaseURL = baseURL
 	return c
 }
 
-func (c *Client) Phone(params map[string]string, timeout time.Duration) (PhoneResponse, error) {
+func (c *Client) Phone(params map[string]string, opts concierge.Options) (PhoneResponse, bool, error) {
 	p := PhoneResponse{}
-	response, err := c.request("phone.json", timeout, params)
+	response, cached, err := c.request("phone.json", params, opts)
 	if err != nil {
-		return p, err
+		return p, cached, err
 	}
 	if err = json.Unmarshal(response, &p); err != nil {
-		return p, err
+		return p, cached, err
 	}
-	return p, err
-
+	return p, cached, err
 }
 
-func (c *Client) Address(params map[string]string, timeout time.Duration) (AddressResponse, error) {
+func (c *Client) Address(params map[string]string, opts concierge.Options) (AddressResponse, bool, error) {
 	p := AddressResponse{}
-	response, err := c.request("location.json", timeout, params)
+	response, cached, err := c.request("location.json", params, opts)
 	if err != nil {
-		return p, err
+		return p, cached, err
 	}
 	if err = json.Unmarshal(response, &p); err != nil {
-		return p, err
+		return p, cached, err
 	}
-	return p, err
-
+	return p, cached, err
 }
 
-func (c *Client) request(method string, timeout time.Duration, params map[string]string) ([]byte, error) {
+// probably no need for this method. Do it all in the above with some common code refactored into functions
+func (c *Client) request(method string, params map[string]string, opts concierge.Options) (res []byte, cached bool, err error) {
+	// Build URL
 	req, _ := url.Parse(c.BaseURL + method)
 	p := url.Values{}
 
 	for k, v := range params {
 		p.Add(k, v)
 	}
-	p.Add("api_key", c.ApiKey)
+	p.Add("api_key", c.APIKey)
 	req.RawQuery = p.Encode()
 
-	// maybe move http client up into struct
-	client := http.Client{
-		Timeout: timeout,
-	}
-	response, err := client.Get(req.String())
-	log.Println(req.String())
-
+	// Build request
+	request, err := http.NewRequest("GET", req.String(), nil)
 	if err != nil {
-		return []byte{}, err
-	} else {
-		defer response.Body.Close()
-		contents, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			return []byte{}, err
-		}
-		e := ErrorResponse{}
-		if err = json.Unmarshal(contents, &e); err != nil {
-			return nil, err
-		}
-		if len(e.Error.Message) > 0 {
-			return nil, errors.New(e.Error.Message)
-		}
-		return contents, err
+		return
 	}
+
+	doer := concierge.NewHTTPDoer(request)
+	res, cached, err = concierge.Request(method, doer, opts)
+	if err != nil {
+		return
+	}
+
+	// defer response.Body.Close()
+
+	// res, err = ioutil.ReadAll(response.Body)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	e := ErrorResponse{}
+	err = json.Unmarshal(res, &e)
+	if err != nil {
+		return
+	}
+
+	if len(e.Error.Message) > 0 {
+		err = errors.New(e.Error.Message)
+		return
+	}
+
+	return
 }
+
+// 	if err != nil {
+// 		return []byte{}, err
+// 	} else {
+// 		defer response.Body.Close()
+// 		contents, err := ioutil.ReadAll(response.Body)
+// 		if err != nil {
+// 			return []byte{}, err
+// 		}
+// 		e := ErrorResponse{}
+// 		if err = json.Unmarshal(contents, &e); err != nil {
+// 			return nil, err
+// 		}
+// 		if len(e.Error.Message) > 0 {
+// 			return nil, errors.New(e.Error.Message)
+// 		}
+// 		return contents, err
+// 	}
+// }
