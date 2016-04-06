93a72f863781fa9be1358c9560d5ca400dee5fc9
Eric Greene <eric.greene@thecontrolgroup.com>
2016-04-06 16:50:33 -0700

    remove notes

--- leads.go
+++ leads.go
@@ -1,12 +1,5 @@
 package whitepages
 
-// sample links
-// name=Drama+Number&phone=6464806649&email_address=medjalloh1@yahoo.com&address.street_line_1=302+Gorham+Ave&
-// address_city=Ashland&address.postal_code=59004&address.state=MT&ip_address=108.194.128.165
-// https://proapi.whitepages.com/3.1/lead_verify_append.json?api_key=3bc9ee9e70576cc0aff8c0fe38a47b5d&firstname=Chester&lastname=Stevens&email_address=dabearsck2013@comcast.net&phone=3095319550&address.street_line_1=706+N.+Lee+St.&address.postal_code=61701&address_city=Bloomington&address.state=IL
-
-// https://proapi.whitepages.com/3.1/lead_verify.json?api_key=3a83ab40280b6926ee62bc50b4de64c4&firstname=Chester&lastname=Stevens&email_address=dabearsck2013@comcast.net&phone=3095319550&address.street_line_1=706+N.+Lee+St.&address.postal_code=61701&address_city=Bloomington&address.state=IL
-
 // Request Fields
 type Request struct {
 	APIKey string `json:"-"`
